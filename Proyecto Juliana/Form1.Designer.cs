b8afd42469cdc9580ffa259c6b345cd5b02db01a
felary <felipe970604@gmail.com>
2024-04-28 14:39:04 -0500

    Agregar archivos de proyecto.

---
+++ Proyecto Juliana/Form1.Designer.cs
@@ -0,0 +1,258 @@
+﻿namespace Proyecto_Juliana
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.imgBender = new System.Windows.Forms.PictureBox();
+            this.imgOvni1Abajo = new System.Windows.Forms.PictureBox();
+            this.imgOvni2Abajo = new System.Windows.Forms.PictureBox();
+            this.imgOvni3Abajo = new System.Windows.Forms.PictureBox();
+            this.imgOvni7Abajo = new System.Windows.Forms.PictureBox();
+            this.imgOvni6Abajo = new System.Windows.Forms.PictureBox();
+            this.imgOvni5Abajo = new System.Windows.Forms.PictureBox();
+            this.imgOvni4Abajo = new System.Windows.Forms.PictureBox();
+            this.imgOvni1Arriba = new System.Windows.Forms.PictureBox();
+            this.imgOvni2Arriba = new System.Windows.Forms.PictureBox();
+            this.imgOvni3Arriba = new System.Windows.Forms.PictureBox();
+            this.imgOvni4Arriba = new System.Windows.Forms.PictureBox();
+            this.imgRespuesta1 = new System.Windows.Forms.PictureBox();
+            this.imgRespuesta2 = new System.Windows.Forms.PictureBox();
+            this.imgRespuesta3 = new System.Windows.Forms.PictureBox();
+            this.ImgPregunta = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.imgBender)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imgOvni1Abajo)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imgOvni2Abajo)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imgOvni3Abajo)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imgOvni7Abajo)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imgOvni6Abajo)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imgOvni5Abajo)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imgOvni4Abajo)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imgOvni1Arriba)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imgOvni2Arriba)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imgOvni3Arriba)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imgOvni4Arriba)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imgRespuesta1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imgRespuesta2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imgRespuesta3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ImgPregunta)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // imgBender
+            // 
+            this.imgBender.Location = new System.Drawing.Point(780, 622);
+            this.imgBender.Name = "imgBender";
+            this.imgBender.Size = new System.Drawing.Size(50, 50);
+            this.imgBender.TabIndex = 0;
+            this.imgBender.TabStop = false;
+            // 
+            // imgOvni1Abajo
+            // 
+            this.imgOvni1Abajo.Location = new System.Drawing.Point(729, 554);
+            this.imgOvni1Abajo.Name = "imgOvni1Abajo";
+            this.imgOvni1Abajo.Size = new System.Drawing.Size(50, 50);
+            this.imgOvni1Abajo.TabIndex = 1;
+            this.imgOvni1Abajo.TabStop = false;
+            // 
+            // imgOvni2Abajo
+            // 
+            this.imgOvni2Abajo.Location = new System.Drawing.Point(517, 554);
+            this.imgOvni2Abajo.Name = "imgOvni2Abajo";
+            this.imgOvni2Abajo.Size = new System.Drawing.Size(50, 50);
+            this.imgOvni2Abajo.TabIndex = 2;
+            this.imgOvni2Abajo.TabStop = false;
+            // 
+            // imgOvni3Abajo
+            // 
+            this.imgOvni3Abajo.Location = new System.Drawing.Point(320, 554);
+            this.imgOvni3Abajo.Name = "imgOvni3Abajo";
+            this.imgOvni3Abajo.Size = new System.Drawing.Size(50, 50);
+            this.imgOvni3Abajo.TabIndex = 3;
+            this.imgOvni3Abajo.TabStop = false;
+            // 
+            // imgOvni7Abajo
+            // 
+            this.imgOvni7Abajo.Location = new System.Drawing.Point(86, 468);
+            this.imgOvni7Abajo.Name = "imgOvni7Abajo";
+            this.imgOvni7Abajo.Size = new System.Drawing.Size(50, 50);
+            this.imgOvni7Abajo.TabIndex = 4;
+            this.imgOvni7Abajo.TabStop = false;
+            // 
+            // imgOvni6Abajo
+            // 
+            this.imgOvni6Abajo.Location = new System.Drawing.Point(224, 468);
+            this.imgOvni6Abajo.Name = "imgOvni6Abajo";
+            this.imgOvni6Abajo.Size = new System.Drawing.Size(50, 50);
+            this.imgOvni6Abajo.TabIndex = 5;
+            this.imgOvni6Abajo.TabStop = false;
+            // 
+            // imgOvni5Abajo
+            // 
+            this.imgOvni5Abajo.Location = new System.Drawing.Point(370, 468);
+            this.imgOvni5Abajo.Name = "imgOvni5Abajo";
+            this.imgOvni5Abajo.Size = new System.Drawing.Size(50, 50);
+            this.imgOvni5Abajo.TabIndex = 6;
+            this.imgOvni5Abajo.TabStop = false;
+            // 
+            // imgOvni4Abajo
+            // 
+            this.imgOvni4Abajo.Location = new System.Drawing.Point(517, 468);
+            this.imgOvni4Abajo.Name = "imgOvni4Abajo";
+            this.imgOvni4Abajo.Size = new System.Drawing.Size(50, 50);
+            this.imgOvni4Abajo.TabIndex = 7;
+            this.imgOvni4Abajo.TabStop = false;
+            // 
+            // imgOvni1Arriba
+            // 
+            this.imgOvni1Arriba.Location = new System.Drawing.Point(846, 327);
+            this.imgOvni1Arriba.Name = "imgOvni1Arriba";
+            this.imgOvni1Arriba.Size = new System.Drawing.Size(50, 50);
+            this.imgOvni1Arriba.TabIndex = 8;
+            this.imgOvni1Arriba.TabStop = false;
+            // 
+            // imgOvni2Arriba
+            // 
+            this.imgOvni2Arriba.Location = new System.Drawing.Point(628, 327);
+            this.imgOvni2Arriba.Name = "imgOvni2Arriba";
+            this.imgOvni2Arriba.Size = new System.Drawing.Size(50, 50);
+            this.imgOvni2Arriba.TabIndex = 9;
+            this.imgOvni2Arriba.TabStop = false;
+            // 
+            // imgOvni3Arriba
+            // 
+            this.imgOvni3Arriba.Location = new System.Drawing.Point(415, 327);
+            this.imgOvni3Arriba.Name = "imgOvni3Arriba";
+            this.imgOvni3Arriba.Size = new System.Drawing.Size(50, 50);
+            this.imgOvni3Arriba.TabIndex = 10;
+            this.imgOvni3Arriba.TabStop = false;
+            // 
+            // imgOvni4Arriba
+            // 
+            this.imgOvni4Arriba.Location = new System.Drawing.Point(75, 327);
+            this.imgOvni4Arriba.Name = "imgOvni4Arriba";
+            this.imgOvni4Arriba.Size = new System.Drawing.Size(50, 50);
+            this.imgOvni4Arriba.TabIndex = 11;
+            this.imgOvni4Arriba.TabStop = false;
+            // 
+            // imgRespuesta1
+            // 
+            this.imgRespuesta1.Location = new System.Drawing.Point(236, 244);
+            this.imgRespuesta1.Name = "imgRespuesta1";
+            this.imgRespuesta1.Size = new System.Drawing.Size(50, 50);
+            this.imgRespuesta1.TabIndex = 12;
+            this.imgRespuesta1.TabStop = false;
+            // 
+            // imgRespuesta2
+            // 
+            this.imgRespuesta2.Location = new System.Drawing.Point(415, 244);
+            this.imgRespuesta2.Name = "imgRespuesta2";
+            this.imgRespuesta2.Size = new System.Drawing.Size(50, 50);
+            this.imgRespuesta2.TabIndex = 13;
+            this.imgRespuesta2.TabStop = false;
+            // 
+            // imgRespuesta3
+            // 
+            this.imgRespuesta3.Location = new System.Drawing.Point(609, 244);
+            this.imgRespuesta3.Name = "imgRespuesta3";
+            this.imgRespuesta3.Size = new System.Drawing.Size(50, 50);
+            this.imgRespuesta3.TabIndex = 14;
+            this.imgRespuesta3.TabStop = false;
+            // 
+            // ImgPregunta
+            // 
+            this.ImgPregunta.Location = new System.Drawing.Point(152, 622);
+            this.ImgPregunta.Name = "ImgPregunta";
+            this.ImgPregunta.Size = new System.Drawing.Size(50, 50);
+            this.ImgPregunta.TabIndex = 15;
+            this.ImgPregunta.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::Proyecto_Juliana.Properties.Resources.fondo;
+            this.ClientSize = new System.Drawing.Size(960, 680);
+            this.Controls.Add(this.ImgPregunta);
+            this.Controls.Add(this.imgRespuesta3);
+            this.Controls.Add(this.imgRespuesta2);
+            this.Controls.Add(this.imgRespuesta1);
+            this.Controls.Add(this.imgOvni4Arriba);
+            this.Controls.Add(this.imgOvni3Arriba);
+            this.Controls.Add(this.imgOvni2Arriba);
+            this.Controls.Add(this.imgOvni1Arriba);
+            this.Controls.Add(this.imgOvni4Abajo);
+            this.Controls.Add(this.imgOvni5Abajo);
+            this.Controls.Add(this.imgOvni6Abajo);
+            this.Controls.Add(this.imgOvni7Abajo);
+            this.Controls.Add(this.imgOvni3Abajo);
+            this.Controls.Add(this.imgOvni2Abajo);
+            this.Controls.Add(this.imgOvni1Abajo);
+            this.Controls.Add(this.imgBender);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.imgBender)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imgOvni1Abajo)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imgOvni2Abajo)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imgOvni3Abajo)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imgOvni7Abajo)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imgOvni6Abajo)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imgOvni5Abajo)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imgOvni4Abajo)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imgOvni1Arriba)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imgOvni2Arriba)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imgOvni3Arriba)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imgOvni4Arriba)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imgRespuesta1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imgRespuesta2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imgRespuesta3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ImgPregunta)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox imgBender;
+        private System.Windows.Forms.PictureBox imgOvni1Abajo;
+        private System.Windows.Forms.PictureBox imgOvni2Abajo;
+        private System.Windows.Forms.PictureBox imgOvni3Abajo;
+        private System.Windows.Forms.PictureBox imgOvni7Abajo;
+        private System.Windows.Forms.PictureBox imgOvni6Abajo;
+        private System.Windows.Forms.PictureBox imgOvni5Abajo;
+        private System.Windows.Forms.PictureBox imgOvni4Abajo;
+        private System.Windows.Forms.PictureBox imgOvni1Arriba;
+        private System.Windows.Forms.PictureBox imgOvni2Arriba;
+        private System.Windows.Forms.PictureBox imgOvni3Arriba;
+        private System.Windows.Forms.PictureBox imgOvni4Arriba;
+        private System.Windows.Forms.PictureBox imgRespuesta1;
+        private System.Windows.Forms.PictureBox imgRespuesta2;
+        private System.Windows.Forms.PictureBox imgRespuesta3;
+        private System.Windows.Forms.PictureBox ImgPregunta;
+    }
+}
+
